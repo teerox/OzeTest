ba8c564cbe9be7712fc271f7cbdc9306476b2154
Mac <mac@Teerox.lan>
2022-11-03 12:40:02 +0100

    first commit

---
+++ OzeTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OzeTest
+//
+//  Created by Mac on 03/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
